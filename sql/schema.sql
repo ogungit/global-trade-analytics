9a1dab9f94eaf063d256d149ffe797d8d44a2e1c
ogungit <saisakethreddykomjan@gmail.com>
2026-01-30 12:29:18 -0600

    Add SQL pipeline (schema) schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,6 @@
 /* =========================================================
    File: schema.sql
    Purpose: Create schema for the Global Trade Analytics project
-   ========================================================= */
-
+   ========================================================= */ 
 CREATE SCHEMA IF NOT EXISTS global_trade;
 
